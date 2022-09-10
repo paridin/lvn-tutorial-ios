e7cfe49c7e7133f5a0043553835b59c03607c885
Roberto Estrada <restrada@paridin.com>
2022-09-10 00:48:31 -0500

    Initial Commit

---
+++ LVNTutorial/LVNTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  LVNTutorialApp.swift
+//  LVNTutorial
+//
+//  Created by Roberto Estrada on 10/09/22.
+//
+
+import SwiftUI
+
+@main
+struct LVNTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
